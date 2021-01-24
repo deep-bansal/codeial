b74f84209c6179a642f1f494c9df082be5593cc1
deep-bansal <deepvinaybansal@yahoo.com>
2021-01-24 15:27:01 +0530

    add partials and layouts

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require("express");
 const app = express();
 const port = 8000;
 const path = require("path");
+const expressLayouts = require("express-ejs-layouts");
 
+app.use(expressLayouts);
 app.use(express.urlencoded());
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));

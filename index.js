efd8367002530b4cb69018e8e84fad4e8b637ff9
deep-bansal <deepvinaybansal@yahoo.com>
2021-01-24 15:51:09 +0530

    add static files

--- index.js
+++ index.js
@@ -4,13 +4,16 @@ const port = 8000;
 const path = require("path");
 const expressLayouts = require("express-ejs-layouts");
 
-app.use(expressLayouts);
-app.use(express.urlencoded());
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+app.set("layout extractStyles", true);
+app.set("layout extractScripts", true);
+app.use(expressLayouts);
+app.use(express.urlencoded());
 
 //use express router
 app.use("/", require("./routes"));
+app.use(express.static("./assets"));
 
 app.listen(port, function (err) {
   if (err) {

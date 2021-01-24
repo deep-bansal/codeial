42a78b43da7b65e7b6ba7db7485b16cef28b7fcb
deep-bansal <deepvinaybansal@yahoo.com>
2021-01-24 16:43:24 +0530

    user sign up

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,5 +1,42 @@
+const User = require("../models/user");
+
 module.exports.profile = function (req, res) {
   return res.render("user_profile", {
     title: "Profie Page",
   });
 };
+
+module.exports.signUp = function (req, res) {
+  return res.render("user_signup", {
+    title: "Codeial | Sign Up",
+  });
+};
+
+module.exports.signIn = function (req, res) {
+  return res.render("user_signin", {
+    title: "Codeial | Sign In",
+  });
+};
+
+module.exports.create = function (req, res) {
+  if (req.body.password != req.body.confirm_password) {
+    return res.redirect("back");
+  }
+  User.findOne({ email: req.body.email }, function (err, user) {
+    if (err) {
+      console.log("error in finding the user in signing up ");
+      return;
+    }
+    if (!user) {
+      User.create(req.body, function (err, user) {
+        if (err) {
+          console.log("error in creating the user while signing up ", err);
+          return;
+        }
+        return res.redirect("/users/signin");
+      });
+    }
+  });
+};
+
+module.exports.createSession = function (req, res) {};
--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require("express");
 const app = express();
 const port = 8000;
 const path = require("path");
+const db = require("./config/mongoose");
 const expressLayouts = require("express-ejs-layouts");
+const cookieParser = require("cookie-parser");
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
@@ -10,6 +12,7 @@ app.set("layout extractStyles", true);
 app.set("layout extractScripts", true);
 app.use(expressLayouts);
 app.use(express.urlencoded());
+app.use(cookieParser());
 
 //use express router
 app.use("/", require("./routes"));

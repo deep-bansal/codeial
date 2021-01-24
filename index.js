243d1f192fbb54a6e7c4fccffc98c696805de969
deep-bansal <deepvinaybansal@yahoo.com>
2021-01-24 15:12:57 +0530

    setup dir

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const app = express();
 const port = 8000;
+const path = require("path");
+
+app.use(express.urlencoded());
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+//use express router
+app.use("/", require("./routes"));
 
 app.listen(port, function (err) {
   if (err) {
